81e735e97c0bb089f003c06392d9ad25956f4208
Devang Vishnu <thedevangvishnu@gmail.com>
2024-04-01 18:24:25 +0530

    Create a Modal component for Register and Login

--- code-with-antonio/airbnb/src/app/layout.tsx
+++ code-with-antonio/airbnb/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Nunito } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/navbar/Navbar";
+import Modal from "./components/modals/Modal";
 
 const nunito = Nunito({
   subsets: ["latin"],
@@ -20,6 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={nunito.className}>
+        <Modal isOpen title="Register" actionLabel="Signup" />
         <Navbar />
         {children}
       </body>
