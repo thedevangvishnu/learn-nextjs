76ad64909ead455d562f68d689d603b2501e17e5
Devang Vishnu <thedevangvishnu@gmail.com>
2024-04-05 14:17:42 +0530

    Create the Card component and add mapbox api key

--- ztm-nextjs/projects/coffee-project/src/app/page.tsx
+++ ztm-nextjs/projects/coffee-project/src/app/page.tsx
@@ -1,9 +1,18 @@
 import Banner from "./components/banner.client";
+import Card from "./components/card.server";
 
 export default function Home() {
+  const coffeeStoreId = "dark-horse-coffee";
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-14">
       <Banner />
+      <div className="w-full grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-2 lg:grid-cols-3 lg:gap-6 mt-6 md:mt-10">
+        <Card
+          name="Dark horse coffee"
+          imgUrl="/static/hero-image.png"
+          href={`/coffee-store/${coffeeStoreId}`}
+        />
+      </div>
     </main>
   );
 }
